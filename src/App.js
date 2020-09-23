f4b5e00758a1b6dfd212169f6ca6e1f2fff9f28c
Umma Ahimsha <uahimsha@gmail.com>
2020-09-24 01:14:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = { myJoke: "" };
+
+  componentDidMount() {
+    this.fetchJoke();
+  }
+
+  fetchJoke = () => {
+    axios
+      .get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" },
+      })
+      .then((response) => {
+        const myJoke = response.data.joke;
+        this.setState({ myJoke });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { myJoke } = this.state;
+    return (
+      <div className="app">
+        <div className="container">
+          <p>😜 Random dad jokes </p>
+          <h1 className="heading">{myJoke}</h1>
+          <button className="button" onClick={this.fetchJoke}>
+            <span>Thank you, next</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
